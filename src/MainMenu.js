7949466e11a3ebaaab4df8caaaa4340ecf821357
Dustin <dustin.tran@live.com>
2018-12-04 12:44:32 +0900

    created basic skeleton for components

---
+++ src/MainMenu.js
@@ -0,0 +1,13 @@
+//contains the main menu to hold selection options
+import React, { Component } from 'react';
+
+export default class MainMenu extends Component {
+
+  render() {
+    return(
+      <div>
+        <text>Place Menu Items here!</text>
+      </div>
+    )
+  }
+}
---
+++ src/Visual.js
@@ -0,0 +1,13 @@
+//Will contain the code to run D3 visualization
+import React, { Component } from 'react';
+
+export default class Visual extends Component {
+
+  render() {
+    return(
+      <div>
+        <text>Place visualization here!</text>
+      </div>
+    )
+  }
+}
