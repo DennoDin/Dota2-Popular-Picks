5a6aef3efc81c5672695c92cbafffe79243433da
Dustin <dustin.tran@live.com>
2018-12-24 17:23:35 +0900

    added and accessed props from parent. conditional render for team name

--- src/Components/MainMenu.jsx
+++ src/Components/MainMenu.jsx
@@ -3,13 +3,30 @@ import React, { Component } from 'react';
 import '../CSS/MainMenu.css'
 
 export default class MainMenu extends Component {
+  constructor(props){
+    super(props);
+  }
 
   render() {
     return(
       <div>
         <h1 className="Title">Dota 2 Popular Heroes</h1>
-        <text>Place Menu Items here!</text>
+        {this.props.selectedTeam === 'None' ? <text>Please select a team!</text> : <text>Selected team is {this.props.selectedTeam}</text>}
       </div>
     )
   }
 }
+/*
+current top teams to implement into choices:
+
+Team Secret
+Virtus Pro
+Evil Geniuses
+PSG.LGD
+OG Dota2
+Team Liquid
+Vici Gaming
+Fnatic
+TNC Predator
+Team Serenity
+*/
