57e3707231ff4d6d561548a466bce95d0c61c016
Dustin <dustin.tran@live.com>
2018-12-24 18:29:12 +0900

    import faux dom and d3 script. rendering d3 script works

--- src/Components/Visual.jsx
+++ src/Components/Visual.jsx
@@ -1,13 +1,23 @@
 //Will contain the code to run D3 visualization
 import React, { Component } from 'react';
+import {withFauxDOM} from 'react-faux-dom'
+import {runD3} from '../D3Script';
 
-export default class Visual extends Component {
+export class Visual extends Component {
+
+  async componentDidMount(){
+    const faux = this.props.connectFauxDOM('div', 'chart');
+    await runD3(faux);
+    this.props.animateFauxDOM(800);
+  }
 
   render() {
     return(
-      <div>
-        <text>Place visualization here!</text>
+      <div className='renderedD3'>
+        {this.props.chart}
       </div>
     )
   }
 }
+
+export default withFauxDOM(Visual);
