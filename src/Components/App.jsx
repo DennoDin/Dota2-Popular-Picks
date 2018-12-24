f58f00e3777887463d8b4bcb2c34eb035a6c5ed6
Dustin <dustin.tran@live.com>
2018-12-24 17:22:29 +0900

    added state and changeTeam to the App component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,10 +3,21 @@ import MainMenu from './MainMenu.jsx'
 import Visual from './Visual.jsx'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedTeam: "None"
+    }
+  }
+
+  changeTeam = () =>{
+    //do something to change team
+  }
+
   render() {
     return (
       <div>
-        <MainMenu/>
+        <MainMenu changeTeam={this.changeTeam} selectedTeam={this.state.selectedTeam}/>
         <Visual/>
       </div>
     );
